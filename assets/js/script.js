e1ad3e1165ab81df4be7d5a6c563882fb460ea03
ImCalebP <91161976+ImCalebP@users.noreply.github.com>
2023-06-01 15:34:42 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = () => navbar.classList.toggle("active");
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = () => navbar.classList.remove("active");
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+/**
+ * VALIDATE FORM AND POP UP CONFIRMAITON
+ */
+function validateForm() {
+  var name = document.forms["appoin-form"]["name"].value;
+  var email = document.forms["appoin-form"]["email_address"].value;
+  var phone = document.forms["appoin-form"]["phone"].value;
+  var category = document.forms["appoin-form"]["category"].value;
+  var date = document.forms["appoin-form"]["date"].value;
+  var message = document.forms["appoin-form"]["message"].value;
+
+  if (name == "" || email == "" || phone == "" || category == "Select category" || date == "" || message == "") {
+    alert("Please fill in all the fields");
+    return false;
+  }
+}
+
+
+/**
+ * filter function
+ */
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all") {
+
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+
+    } else {
+
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+
+    }
+  }
+}
+
+addEventOnElem(filterBtns, "click", filter);
+
